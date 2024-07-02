730507e5385e0649ffc3be5169f3c41b972054b3
windofnet <windofnet@hotmail.com>
2024-07-02 11:54:31 +0800

    icon

--- src/lib/GachaDrawHistory.svelte
+++ src/lib/GachaDrawHistory.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { type GachaDrawResult } from '$lib';
   import _ from 'lodash';
+  import fe from 'feather-icons';
 
   enum display {
     hist = 1,
@@ -9,6 +10,9 @@
 
   export let gachaDrawHistory: GachaDrawResult[] = [];
   export let displayType = display.hist;
+
+  let r1: HTMLInputElement;
+  let r2: HTMLInputElement;
 
   $: over5000 = gachaDrawHistory.length > 5000;
   $: analysisData = _.chain(gachaDrawHistory)
@@ -23,23 +27,40 @@
 </script>
 
 <div class="flex flex-col gap-2">
+  <input
+    bind:this={r1}
+    class="hidden"
+    type="radio"
+    name="type"
+    value={display.hist}
+    bind:group={displayType}
+  />
+  <input
+    bind:this={r2}
+    class="hidden"
+    type="radio"
+    name="type"
+    value={display.analysis}
+    bind:group={displayType}
+  />
+
   <div class="join">
-    <input
-      class="join-item btn"
-      type="radio"
-      name="options"
-      aria-label="顯示紀錄"
-      value={display.hist}
-      bind:group={displayType}
-    />
-    <input
-      class="join-item btn"
-      type="radio"
-      name="options"
-      aria-label="顯示統計"
-      value={display.analysis}
-      bind:group={displayType}
-    />
+    <button
+      class="btn join-item"
+      class:btn-primary={displayType === display.hist}
+      on:click={() => r1.click()}
+    >
+      {@html fe.icons['list'].toSvg({ class: 'w-5 h-5' })}
+      顯示紀錄
+    </button>
+    <button
+      class="btn join-item"
+      class:btn-primary={displayType === display.analysis}
+      on:click={() => r2.click()}
+    >
+      {@html fe.icons['activity'].toSvg({ class: 'w-5 h-5' })}
+      顯示統計
+    </button>
   </div>
 
   {#if displayType === display.hist}
