5679dccc9d67b6a45ae73cce2561b3444cbae2c3
windofnet <windofnet@hotmail.com>
2024-07-17 19:10:24 +0800

    update gacha

--- src/gachas/index.ts
+++ src/gachas/index.ts
@@ -12,6 +12,7 @@ import gacha202405_1 from './2024_5月轉蛋Ⅰ.json';
 import gacha202405_2 from './2024 5月轉蛋Ⅱ.json';
 import gacha202406_1 from './2024 6月轉蛋Ⅰ.json';
 import gacha202406_2 from './2024 6月轉蛋Ⅱ.json';
+import gacha202407_1 from './2024 7月轉蛋I.json';
 import 柴語錄轉蛋 from './柴語錄轉蛋.json';
 import 雪季轉蛋 from './雪季轉蛋.json';
 import _2024劍指ROS轉蛋 from './2024劍指ROS轉蛋.json';
@@ -32,7 +33,8 @@ const gachas: Gacha[] = [
   柴語錄轉蛋,
   gacha202406_1,
   gacha202406_2,
-  _2024劍指ROS轉蛋
+  _2024劍指ROS轉蛋,
+  gacha202407_1
 ];
 
 export default gachas;
