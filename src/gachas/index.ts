0aa13a6a5b1ac3ba87dc4d52d140511831ec2106
windofnet <windofnet@hotmail.com>
2025-06-04 14:32:17 +0800

    update gacha

--- src/gachas/index.ts
+++ src/gachas/index.ts
@@ -38,6 +38,7 @@ import gacha202504_1 from './2025_4月轉蛋Ⅰ.json';
 import gacha202504_2 from './2025 4月轉蛋Ⅱ.json';
 import gacha202505_1 from './2025 5月轉蛋Ⅰ.json';
 import gacha202505_2 from './2025 5月轉蛋Ⅱ.json';
+import gacha202506_1 from './2025 6月轉蛋I.json';
 
 const gachas: Gacha[] = [
   雪季轉蛋,
@@ -78,7 +79,8 @@ const gachas: Gacha[] = [
   gacha202504_1,
   gacha202504_2,
   gacha202505_1,
-  gacha202505_2
+  gacha202505_2,
+  gacha202506_1
 ];
 
 export default gachas;
